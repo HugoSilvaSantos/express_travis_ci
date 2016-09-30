962330c564bb57598e305746f6fc6e825cd28baf
Hugo Santos <hugosilva_santos@hotmail.com>
2016-10-01 00:19:52 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + server.address().port);  
+});
+
+// routes
+app.get('/', function(req, res) {
+    res.send('TEST!!');
+});
+app.get('/home', function(req, res) {
+    res.send({'Message':'OK' });
+});
